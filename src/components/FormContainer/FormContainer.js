a9ec1b6281d5c1374550e40ea61eb58bfa2fd1ba
shelchkovy <shelchkovy@yandex.ru>
2019-10-21 19:55:10 +0400

    Changed HOC logic

--- src/components/FormContainer/FormContainer.js
+++ src/components/FormContainer/FormContainer.js
@@ -4,17 +4,13 @@ import styled from 'styled-components';
 
 // Centered container for the form with border and shadow
 const Container = styled.div`
-	border: 1px solid #AAC3BE;
-	border-radius: .5rem;
-	box-shadow: 0px 0px 38px -5px rgba(204,204,204,0.46);
 	max-width: 286px;
-	padding: 2rem;
-	width: 50%;
-	margin-left: auto;
-	margin-right: auto;
 
-	@media screen and (min-width: 30em) {
-		width: 40%;
+	@media (min-width: 501px) {
+		border: 1px solid #AAC3BE;
+		border-radius: .5rem;
+		box-shadow: 0px 0px 38px -5px rgba(204,204,204,0.46);
+		padding: 2rem;
 	}
 `;
 
@@ -22,28 +18,26 @@ const Container = styled.div`
 const WrapDiv = styled.div`
 	height: 100vh;
 	display: flex;
-	flex-direction: column;
 	justify-content: center;
+	align-items: center;
+	margin-left: auto;
+	margin-right: auto;
+
+	@media (max-width: 500px) {
+		max-width: 240px;
+	}
 `;
 
 function FormContainer(WrappedComponent) {
 	return class extends React.Component {
 		render() {
-			if(window.innerWidth > 500) {
-				return (
-					<WrapDiv>
-						<Container>
-							<WrappedComponent {...this.props} />
-						</Container>
-					</WrapDiv>
-				);
-			} else {
-				return (
-					<div className='vh-100 flex flex-column justify-center mw-240 center'>
+			return (
+				<WrapDiv>
+					<Container>
 						<WrappedComponent {...this.props} />
-					</div>
-				);
-			}
+					</Container>
+				</WrapDiv>
+			);
 		}
 	};
 }
