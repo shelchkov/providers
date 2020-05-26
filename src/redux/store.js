83bf5cf92123fc868f40e0e705e89bf4749def63
shelchkovy <shelchkovy@yandex.ru>
2020-05-26 18:59:19 +0400

    Hide redux messages in production #50

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './root-reducer';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux"
+import rootReducer from "./root-reducer"
+import logger from "redux-logger"
 
-const middlewares = [logger];
+const middlewares = []
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+// eslint-disable-next-line
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger)
+}
+
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewares)
+)
