33f69895497dca5872788d1119e4468a50f91bc4
shelchkovy <shelchkovy@yandex.ru>
2020-06-01 10:01:39 +0400

    Add mobile logos #23

--- src/utils/providers-list.js
+++ src/utils/providers-list.js
@@ -1,5 +1,15 @@
 export const providersList = [
-	{name: "Megafon", logo: "./images/megafon.png", id: 0},
-	{name: "MTS", logo: "./images/mts.svg", id: 1},
-	{name: "Beeline", logo: "./images/beeline.png", id: 2},
+  {
+    name: "Megafon",
+    logo: "./images/megafon.png",
+    mobileLogo: "./images/megafon-mobile.png",
+    id: 0,
+  },
+  { name: "MTS", logo: "./images/mts.svg", id: 1 },
+  {
+    name: "Beeline",
+    logo: "./images/beeline.png",
+    mobileLogo: "./images/beeline-mobile.png",
+    id: 2,
+  },
 ]
