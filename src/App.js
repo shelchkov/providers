00c3bd5edab1b4ca53eb44e03462a055bb422fea
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2021-08-22 09:37:30 +0400

    refactor: refactor providers list and main component #92

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react"
-import "./App.css"
-import { ProvidersList } from "./components/ProvidersList/ProvidersList"
-import Form from "./components/Form/Form"
 import styled from "styled-components"
 import { connect } from "react-redux"
 import { createStructuredSelector } from "reselect"
 import { Switch, Route, withRouter } from "react-router-dom"
-
-import { providersList } from "./utils/providers-list"
-import { sendRequest, urlParams } from "./utils/utils"
-import { startSubmit, submitSuccess, resetForm, submitFail } from "./redux/form/form.actions"
-import { selectCanSubmit } from "./redux/form/form.selectors"
 import { compose } from "redux"
 import { withScreenCover } from "screen-cover"
+
+import { ProvidersList } from "./components/ProvidersList/ProvidersList"
+import Form from "./components/Form/Form"
+
+import { providersList } from "./utils/providers-list"
+import { requestInfo } from "./utils/api-utils"
+import { startSubmit, submitSuccess, submitFail } from "./redux/form/form.actions"
+import { selectCanSubmit } from "./redux/form/form.selectors"
+import { getProvider } from "./utils/utils"
 
 const AppDiv = styled.div`
   text-align: center;
@@ -21,17 +22,11 @@ const AppDiv = styled.div`
 class App extends React.PureComponent {
   constructor() {
     super()
-    this.state = {
-      selectedProvider: {},
-    }
+    this.state = { selectedProvider: {} }
   }
 
   setProvider = (providerId) => {
-    let selectedProvider
-
-    if (Number.isFinite(providerId)) {
-      selectedProvider = providersList.find(({ id }) => id === providerId)
-    }
+    const selectedProvider = getProvider(providersList, providerId)
 
     if (!selectedProvider) {
       this.props.history.push("/")
@@ -40,57 +35,24 @@ class App extends React.PureComponent {
     this.setState({ selectedProvider })
   }
 
-  selectProvider = (providerId) => {
-    this.setProvider(providerId)
-    this.props.resetForm()
-    this.cover("form")
-  }
-
-  submitForm = (formData) => {
+  submitForm = async (formData) => {
     if (!this.props.canSubmit) {
       return
     }
     
     this.props.startSubmit()
 
-    this.requestInfo(formData)
-      .then((res) => {
-        console.log(res)
-        this.props.submitSuccess()
-        this.getHome()
-      })
-      .catch((error) => {
-        console.log("Error: " + error.message)
-        this.props.submitFail(error.message)
-      })
-  }
-
-  cover = (route) => {
-    this.props.showCover(() => {
-      this.props.history.push(
-        route === "home"
-          ? ""
-          : `${route}?${urlParams.provider}=${this.state.selectedProvider.id}`
-      )
-    })
-  }
-
-  async requestInfo(formData) {
-    if (formData.phone.length !== 11 || !formData.amount) {
-      throw new Error("Wrong Data provided!")
+    try {
+      const response = await requestInfo(formData, this.state.selectedProvider)
+      
+      this.props.submitSuccess(response)
+      this.goHome()
+    } catch (error) {
+      this.props.submitFail(error.message)
     }
-
-    const result = await sendRequest(
-      this.state.selectedProvider,
-      formData
-    )
-
-    return result
   }
 
-  getHome = () => {
-    this.cover("home")
-  }
+  goHome = () => this.props.showCover(() => this.props.history.push("/"))
 
   render() {
     return (
@@ -99,9 +61,7 @@ class App extends React.PureComponent {
           <Route
             exact
             path="/"
-            render={() => (
-              <ProvidersList selectProvider={this.selectProvider} />
-            )}
+            render={() => <ProvidersList setProvider={this.setProvider} />}
           />
           
           <Route
@@ -109,7 +69,7 @@ class App extends React.PureComponent {
             path="/form"
             render={() => (
               <Form
-                getHome={this.getHome}
+                getHome={this.goHome}
                 submitForm={this.submitForm}
                 provider={this.state.selectedProvider}
                 setProvider={this.setProvider}
@@ -127,9 +87,8 @@ const mapStateToProps = createStructuredSelector({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  resetForm: () => dispatch(resetForm()),
   startSubmit: () => dispatch(startSubmit()),
-  submitSuccess: () => dispatch(submitSuccess()),
+  submitSuccess: (response) => dispatch(submitSuccess(response)),
   submitFail: (error) => dispatch(submitFail(error)),
 })
 
